439351097ca9a9110f2a4cf688515934848894a5
ling10344 <148197611+ling10344@users.noreply.github.com>
2023-12-22 14:14:59 +0800

    Update NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React from 'react';
 
 const NavBar = () => {
+  const handleEventClick = () => {
+    // Fetch events from the events API and add them to the calendar
+  };
+
+  const handleHolidayClick = () => {
+    // Fetch holidays from the holidays API and add them to the calendar
+  };
+
+  const handleTodoClick = () => {
+    // Manage showing and adding to-dos
+  };
+
   return (
-    <div className="navBar">
-      {/* Add your navigation items here */}
-      <ul>
-        <li>Holiday</li>
-        <li>To-do-list</li>
-        <li>Diary</li>
-        <li>Event in HK</li>
-      </ul>
-    </div>
+    <nav>
+      <button onClick={handleEventClick}>Event</button>
+      <button onClick={handleHolidayClick}>Holiday</button>
+      <button onClick={handleTodoClick}>TodoList</button>
+    </nav>
   );
 };
 
