33b90dcfef5cfc5f3b9ddb01488609fb48dd0e72
allycly2 <148197506+allycly2@users.noreply.github.com>
2023-12-27 10:47:08 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,19 +7,17 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="top-navigation-bar">
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/243/243925.png"
-          className="logo"
-        ></img>
-        <h1>Online Calendar</h1>
-      </div>
-
       <div className="navBar-container">
         <NavBar />
       </div>
-      <div className="calendar-container">
-        <Calendar />
+      <div className="main">
+        <div className="view-button">
+          <button>Month</button>
+          <button>Year</button>
+        </div>
+        <div className="calendar-container">
+          <Calendar />
+        </div>
       </div>
     </div>
   );
